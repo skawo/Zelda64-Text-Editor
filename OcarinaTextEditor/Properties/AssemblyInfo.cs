ee26480369d6b24530558902ba2b8a9a2d9f24c9
skawo <43761362+skawo@users.noreply.github.com>
2024-03-31 14:09:02 +0200

    v3.25

--- OcarinaTextEditor/Properties/AssemblyInfo.cs
+++ OcarinaTextEditor/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.22")]
-[assembly: AssemblyFileVersion("3.22")]
+[assembly: AssemblyVersion("3.25")]
+[assembly: AssemblyFileVersion("3.25")]
